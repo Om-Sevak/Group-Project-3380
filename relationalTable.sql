6a98f73df37333cbd3b3dc501c47910bcaa94d50
Om Sevak <omsevak252002@gmail.com>
2022-11-14 18:02:47 -0600

    Completed relational schema

--- relationalTable.sql
+++ relationalTable.sql
@@ -1,3 +1,17 @@
+drop table if exists madeIn;
+drop table if exists mediaGenre;
+drop table if exists castInvolved;
+drop table if exists directedBy;
+drop table if exists streamedOn;
+drop table if exists isA;
+drop table if exists mediaType;
+drop table if exists platform;
+drop table if exists cast;
+drop table if exists genre;
+drop table if exists country;
+drop table if exists director;
+drop table if exists entertainment;
+
 CREATE TABLE entertainment(
     mediaID integer primary key IDENTITY(1,1),
     mediaName VARCHAR(1000) UNIQUE not NULL,
@@ -6,37 +20,37 @@ CREATE TABLE entertainment(
     rottenTomatoes integer check (rottenTomatoes >= 0 AND rottenTomatoes <= 100),
     IMDB DECIMAL (2, 1) check (IMDB >= 0 AND IMDB <= 100),
     duration INTEGER check (duration > 0),
-    mediaDescription VARCHAR
+    mediaDescription VARCHAR (1000)
 );
 
 CREATE TABLE director(
     dirID integer primary key IDENTITY(1,1),
-    dirName VARCHAR UNIQUE not NULL
+    dirName VARCHAR(1000) UNIQUE not NULL
 );
 
 CREATE TABLE country(
     countryID integer primary key IDENTITY(1,1),
-    countryName VARCHAR UNIQUE not NULL
+    countryName VARCHAR(1000) UNIQUE not NULL
 );
 
 CREATE TABLE genre(
     genreID integer primary key IDENTITY(1,1),
-    genreName VARCHAR UNIQUE not NULL
+    genreName VARCHAR(1000) UNIQUE not NULL
 );
 
 CREATE TABLE cast(
     castID integer primary key IDENTITY(1,1),
-    castName VARCHAR UNIQUE not NULL
+    castName VARCHAR(1000) UNIQUE not NULL
 );
 
 CREATE TABLE platform(
     platformID integer primary key IDENTITY(1,1),
-    platformName VARCHAR UNIQUE 
+    platformName VARCHAR(1000) UNIQUE 
 );
 
 CREATE TABLE mediaType(
     mediaTypeID integer primary key IDENTITY(1,1),
-    mediaTypeName VARCHAR UNIQUE not NULL
+    mediaTypeName VARCHAR(1000) UNIQUE not NULL
 );
 
 CREATE TABLE isA(
@@ -47,7 +61,7 @@ CREATE TABLE isA(
 CREATE TABLE streamedOn(
     mediaID integer REFERENCES entertainment(mediaID),
     platformID integer REFERENCES platform(platformID),
-    dateAdded VARCHAR
+    dateAdded VARCHAR(1000)
     PRIMARY KEY(mediaID, platformID)
 );
 
@@ -76,10 +90,11 @@ CREATE TABLE mediaGenre(
 );
 
 --These are fixed media type
-INSERT into mediaType VALUES (0, "Movie");
-INSERT into mediaType VALUES (1, "Tv-Show");
+INSERT into mediaType (mediaTypeName) VALUES ('Movie');
+INSERT into mediaType (mediaTypeName) VALUES ('Tv-Show');
 --These are fixed platforms
-INSERT INTO platform VALUES (0, "Netflix");
-INSERT INTO platform VALUES (1, "Hulu");
-INSERT INTO platform VALUES (2, "Disney Plus");
-INSERT INTO platform VALUES (3, "Amazon Prime");
+INSERT INTO platform (platformName) VALUES ('Netflix');
+INSERT INTO platform (platformName) VALUES ('Amazon Prime');
+INSERT INTO platform (platformName) VALUES ('Disney Plus');
+INSERT INTO platform (platformName) VALUES ('Hulu');
+
