f8ea62d4d3950db0015be18447927158de78d2b8
Adwait Pujari <adwaitpujari08@gmail.com>
2022-11-21 20:46:41 -0600

    Testing Queries

--- test.sql
+++ test.sql
@@ -24,3 +24,58 @@
 --     group by mediaID
 --     having count(*) = 3 
 -- )
+
+-- Movies with more than two genres
+--    with temp ( mediaId, mediaName, genreID) as 
+--     (
+--         SELECT entertainment.mediaID, entertainment.mediaName, count(mediaGenre.genreID) FROM entertainment 
+--         join mediaGenre on entertainment.mediaID = mediaGenre.mediaID
+--         group by mediaGenre.mediaID, entertainment.mediaName, entertainment.mediaID
+--         having COUNT(*) > 2
+--     )
+
+--     SELECT  temp.mediaName, genre.genreName from temp 
+--     join mediaGenre on temp.mediaId = mediaGenre.mediaID
+--     join genre on mediaGenre.genreID = genre.genreID
+--     order by temp.mediaID;
+
+-- SELECT mediaId, mediaName from entertainment
+-- where IMDB > (
+--     SELECT  AVG(IMDB)
+--     from 
+--     entertainment join streamedOn on entertainment.mediaID = streamedOn.mediaID
+--     where platformID = 1
+--     GROUP BY platformID
+--     having AVG(IMDB) > 0
+-- );
+
+-- SELECT mediaID, IMDB from entertainment
+-- where IMDB < 5
+-- order by IMDB DESC;
+
+-- Movies made in a particular country
+    -- select mediaName
+    -- from entertainment
+    -- join isA on entertainment.mediaId = isA.mediaId
+    -- join madeIn on isA.mediaID = madeIn.mediaId
+    -- join country on madeIn.countryID = country.countryID
+    -- where country.countryName = 'Pakistan' and  isA.mediaTypeID = 1 ;
+
+-- with moviesInCountry (mediaName, IMDB) as
+--     (select mediaName, IMDB
+--     from entertainment
+--     join isA on entertainment.mediaId = isA.mediaId
+--     join madeIn on isA.mediaID = madeIn.mediaId
+--     join country on madeIn.countryID = country.countryID
+--     where country.countryName = 'Pakistan' and isA.mediaTypeID = 1 )
+
+--     Select top 5 mediaName, IMDB
+--     from moviesInCountry 
+--     order by IMDB desc;
+
+-- SELECT entertainment.mediaID, entertainment.mediaName, COUNT(countryID) as noOfCountries FROM 
+-- entertainment join madeIn on entertainment.mediaID = madeIn.mediaID
+-- join isA on entertainment.mediaID = isA.mediaID
+-- where mediaTypeID = 1 
+-- GROUP BY entertainment.mediaName, entertainment.mediaID
+-- having COUNT(countryID)> 2;
